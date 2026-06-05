1f5fd3262bf257e780a878601261ba60f14f8a1b
3jikeanekwe <236617477+3jikeanekwe@users.noreply.github.com>
2026-06-05 14:32:08 +0100

    Create layout.tsx

---
+++ src/app/dashboard/admin/layout.tsx
@@ -0,0 +1,9 @@
+import AdminProtected from "@/components/admin-protected";
+
+export default function AdminLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return <AdminProtected>{children}</AdminProtected>;
+}
