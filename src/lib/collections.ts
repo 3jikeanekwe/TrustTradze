59d9ebfc565deb162d4b87c4a7fe159106f4708f
3jikeanekwe <236617477+3jikeanekwe@users.noreply.github.com>
2026-06-05 14:05:05 +0100

    Create collections.ts

---
+++ src/lib/collections.ts
@@ -0,0 +1,17 @@
+export const COLLECTIONS = {
+  USERS: "users",
+
+  NOTIFICATIONS: "notifications",
+
+  ADMIN_LOGS: "admin_logs",
+
+  PRODUCTS: "products",
+
+  SERVICES: "services",
+
+  ESCROWS: "escrows",
+
+  MESSAGES: "messages",
+
+  DISPUTES: "disputes"
+} as const;
